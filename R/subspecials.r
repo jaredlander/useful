30131678400181a9549933456cdeb631dbcdb8cc
Jared Lander <info@jaredlander.com>
2011-10-15 19:55:06 -0400

    simpler version of subSpecials and subOut which take just a vector of special characters and it adds the slashes to the subout characters and the sub in characters

--- R/subspecials.r
+++ R/subspecials.r
@@ -7,24 +7,23 @@
 ## loops through all the special characters, subbing them out one by one with their escaped equivalent
 ## @toAlter:  vector of words to have their special characters subbed out
 ## @specialChars: the characters to be replaced
-## @modChars: new version of characters
 ## returns the modified vector
-subOut <- function(toAlter, specialChars=c("\\!", "\\(", "\\)", "\\-", "\\=", "\\*", "\\."), 
-                        modChars=c("\\\\!", "\\\\(", "\\\\)", "\\\\-", "\\\\=", "\\\\*", "\\\\."))
+
+subOut <- function(toAlter, specialChars=c("!", "(", ")", "-", "=", "*", "."))
 {
-  # make sure the special characters has the same length as the replacement characters
-  if(length(specialChars) != length(modChars))
-  {
-    stop("specialChars and modChars must be the same length", call.=FALSE)
-  }
+    # put slashes in front of the characters
+    specialChars <- paste("\\", specialChars, sep="")
+    
+    # put double slashes for the replacements
+    modChars <- paste("\\", specialChars, sep="")
   
-  # loop through the special characters and sub in the replacements
-  for(i in 1:length(specialChars))
+    # loop through the special characters and sub in the replacements
+    for(i in 1:length(specialChars))
     {
-    	toAlter <- gsub(specialChars[i], modChars[i], toAlter)    # do the subbing
-  }
+        toAlter <- gsub(specialChars[i], modChars[i], toAlter)    # do the subbing
+    }
   
-  return(toAlter)
+    return(toAlter)
 }
 
 
@@ -32,17 +31,9 @@ subOut <- function(toAlter, specialChars=c("\\!", "\\(", "\\)", "\\-", "\\=", "\
 ## Meant to help out when doing regular expressions
 ## @...: 1 to n vectors to be subbed on
 ## @specialChars: the characters to be replaced
-## @modChars: new version of characters
 ## calls .subOut to do the actual work
 ## returns list of the modified vectors
-subSpecials <- function(..., specialChars=c("\\!", "\\(", "\\)", "\\-", "\\=", "\\*"), 
-                        modChars=c("\\\\!", "\\\\(", "\\\\)", "\\\\-", "\\\\=", "\\\\*"), simplify=FALSE)
+subSpecials <- function(..., specialChars=c("!", "(", ")", "-", "=", "*", "."), simplify=FALSE)
 {
-  # make sure the special characters has the same length as the replacement characters
-  if(length(specialChars) != length(modChars))
-  {
-    stop("specialChars and modChars must be the same length", call.=FALSE)
-  }
-    
-    return(llply(list(...), subOut, specialChars=specialChars, modChars))  # run .subOut on each vector, returning the resulting list
+    return(llply(list(...), subOut, specialChars=specialChars))  # run .subOut on each vector, returning the resulting list
 }
