9230ce3b87e6e6e3bf7ed78a26f34a8f90637067
jaredlander <info@jaredlander.com>
2013-03-25 16:30:08 -0400

    builx.d now has an option to build factors without contrasts.

--- R/buildMatrix.r
+++ R/buildMatrix.r
@@ -8,17 +8,76 @@
 #' @export build.x
 #' @param formula A formula
 #' @param data A data.frame
+#' @param contrasts Logical indicating whether a factor's base level is removed.  For now only a single value should be supplied.  In the future it will be either one single value applied to every factor or a value for each factor.  Values will be recycled if necessary.
+#' @param sparse Logical indicating if result should be sparse.  Currently not used.
 #' @return A matrix of the predictor variables specified in the formula
 #' @examples
 #' require(ggplot2)
 #' head(mpg)
 #' head(build.x(hwy ~ class + cyl + year, data=mpg))
 #' 
-build.x <- function(formula, data)
+#' testFrame <- data.frame(First=sample(1:10, 20, replace=TRUE), Second=sample(1:20, 20, replace=TRUE), Third=sample(1:10, 20, replace=TRUE), Fourth=factor(rep(c("Alice","Bob","Charlie","David"), 5)), Fifth=ordered(rep(c("Edward","Frank","Georgia","Hank","Isaac"), 4)), Sixth=factor(rep(c("a", "b"), 10)), stringsAsFactors=F)
+#' head(build.x(First ~ Second + Fourth + Sixth, testFrame, contrasts=c("Fourth"=TRUE, "Fifth"=FALSE, "Sixth"=TRUE)))
+#' head(build.x(First ~ Second + Fourth + Fifth + Sixth, testFrame, contrasts=c(Fourth=TRUE, Fifth=FALSE, Sixth=TRUE)))
+#' head(build.x(First ~ Second + Fourth + Fifth + Sixth, testFrame, contrasts=TRUE))
+#' head(build.x(First ~ Second + Fourth + Fifth + Sixth, testFrame, contrasts=FALSE))
+#' head(build.x(First ~ Second + Fourth + Fifth + Sixth - 1, testFrame, contrasts=TRUE))
+#' head(build.x(First ~ Second + Fourth + Fifth + Fourth*Sixth, testFrame, contrasts=TRUE))
+#' head(build.x(First ~ Second + Fourth + Fifth + Third*Sixth, testFrame, contrasts=TRUE))
+#' #' head(build.x(First ~ Second + Fourth + Fifth + Fourth*Sixth, testFrame, contrasts=FALSE))
+#' head(build.x(First ~ Second + Fourth + Fifth + Third*Sixth, testFrame, contrasts=FALSE))
+#' 
+#' ## if contrasts is a list then you can specify just certain factors
+build.x <- function(formula, data, contrasts=TRUE, sparse=FALSE)
 {
-    model.matrix(formula, data=ForceDataFrame(data))[, -1]
+    # ensure data is a data.frame
+    data <- ForceDataFrame(data)
+        
+    # make index of factor or character columns
+    catIndex <- which(sapply(data, function(x) is.factor(x) | is.character(x)))
+    # only keep those that also appear in the factors attr of the terms of formula
+    theTerms <- rownames(attr(terms(formula, data=data), "factors"))
+    # new cat index only keeping those variables that are necessary
+    catIndex <- catIndex[which(names(data)[catIndex] %in% theTerms)]
+    # also cut down contrasts argument
+    # save for another time
+    
+    # if any of these identified columns is still a character, they need to be changed into a factor
+    # find out which columns are characters
+    #print(sapply(data[, catIndex], is.character))
+    charIndex <- catIndex[sapply(data[, catIndex], is.character)]
+    
+    # convert to factor
+    data[, charIndex] <- catcolwise(as.factor)(data[, charIndex, drop=FALSE])
+    ## now all factors or characters are at least factors (and nothing extraneous was done) and only the appropriate columns will be put into the contrasts argument
+    
+    # if multiple contrasts are given they must be named
+    contrNames <- names(contrasts)
+    if(length(contrasts) > 1 && is.null(contrNames))
+    {
+        stop("If specifying more than one contrasts then it must be a named list of vector.")
+    }else if(!is.null(contrNames))
+    {
+        # get names of contrasts and use as the catIndex, factor/ordered columns not specified will be left to the default
+        catIndex <- contrNames
+    }else if(length(contrasts) == 1)
+    {
+        # make as many contrasts as necessary
+        contrasts <- rep(contrasts, times=length(catIndex))
+    }
+    
+    # only non sparse is allowed for now
+    sparse <- FALSE
+    # build contrast argument list
+    #contrArgs <- lapply(data[, catIndex, drop=FALSE], contrasts, contrasts=contrasts, sparse=sparse)
+    contrArgs <- mapply(contrasts, data[, catIndex, drop=F], contrasts, MoreArgs=list(sparse=sparse))
+    
+    # build model.matrix
+    model.matrix(formula, data=data, contrasts.arg=contrArgs)#[, -1])
+    #model.matrix(formula, data=data)[, -1]
 }
-
+#mapply(function(input, contrasts, sparse=FALSE){ contrasts(x=input, contrasts=contrasts, sparse=sparse) }, testFrame[, 4:5, drop=F], c(T), MoreArgs=list(sparse=F))
+#head(model.matrix(~ ., data=testFrame, ))
 #' ForceDataFrame
 #' 
 #' Force matrix and arrays to data.frame
