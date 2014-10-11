b9180fbfe0cb8b0cfa0f3f84e9cd90952125f657
jkraut <joshua.kraut@gmail.com>
2014-10-11 19:03:20 -0400

    Update kmeansPlotting.r
    added optional argument for point size in plot.kmeans function

--- R/kmeansPlotting.r
+++ R/kmeansPlotting.r
@@ -69,6 +69,7 @@ fortify.kmeans <- function(model, data=NULL, ...)
 #' @param x A \code{\link{kmeans}} object.
 #' @param data The data used to kit the \code{\link{kmeans}} object.
 #' @param class Character name of the "true" classes of the data.
+#' @param PtSize Numeric size of ggplot points
 #' @param legend.position Character indicating where the legend should be placed.
 #' @param title Title for the plot.
 #' @param xlab Label for the x-axis.
@@ -81,22 +82,24 @@ fortify.kmeans <- function(model, data=NULL, ...)
 #' plot(k1)
 #' plot(k1, data=iris)
 #' 
-plot.kmeans <- function(x, data=NULL, class=NULL, legend.position=c("right", "bottom", "left", "top", "none"), 
-                        title="K-Means Results",
-                        xlab="Principal Component 1", ylab="Principal Component 2", ...)
+plot.kmeans <-
+    function(x, data=NULL, class=NULL, PtSize=2, legend.position=c("right", "bottom", "left", "top", "none"), 
+                          title="K-Means Results",
+                          xlab="Principal Component 1", ylab="Principal Component 2", ...)
 {
-    # fortify the model and data so it is convenient to plot in ggplot
-    toPlot <- fortify(model=x, data=data)
+      # fortify the model and data so it is convenient to plot in ggplot
+      toPlot <- fortify(model=x, data=data)
     
-    # get the legend position
-    legend.position <- match.arg(legend.position)
+      # get the legend position
+      legend.position <- match.arg(legend.position)
     
-    # convert class to factor just in case it is not already
-    if(!is.null(class)) toPlot[, class] <- factor(toPlot[, class])
+      # convert class to factor just in case it is not already
+      if(!is.null(class)) toPlot[, class] <- factor(toPlot[, class])
     
-    ggplot(toPlot, aes(x=.x, y=.y, colour=.Cluster)) + 
-        geom_point(aes_string(shape=class)) + 
-        scale_color_discrete("Cluster") +
-        theme(legend.position=legend.position) +
-        labs(title=title, x=xlab, y=ylab)
+      ggplot(toPlot, aes(x=.x, y=.y, colour=.Cluster)) + 
+          geom_point(aes_string(shape=class), size = PtSize) + 
+          scale_color_discrete("Cluster") +
+          theme(legend.position=legend.position) +
+          labs(title=title, x=xlab, y=ylab)
 }
+
