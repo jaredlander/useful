37503ad8a006372170e6c36dcd5f3a563cb9c001
Jared Lander <info@jaredlander.com>
2018-10-07 19:13:07 -0400

    Skipping package tests on travis

--- tests/testthat/test-load-packages.R
+++ tests/testthat/test-load-packages.R
@@ -9,12 +9,14 @@ testthat::teardown({
 
 test_that("Packages load as expected", {
     testthat::skip_on_cran()
+    testthat::skip_on_travis()
     expect_message(load_packages('ggplot2'), 'ggplot2')
     expect_message(load_packages(c('ggplot2', 'coefplot')), 'ggplot2, coefplot')
 })
 
 test_that('Errors are captured', {
     testthat::skip_on_cran()
+    testthat::skip_on_travis()
     expect_error(load_packages(5), 'packages is not a character vector')
     expect_error(load_packages(c('ggplot2', 'fake1')), 'fake1')
 })
