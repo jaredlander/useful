28e77069e72fb960718db7c8f394b22dda954f38
Jared Lander <info@jaredlander.com>
2022-03-17 21:54:04 -0400

    Adding MapToInterval changes

--- R/MapToInterval.r
+++ R/MapToInterval.r
@@ -14,6 +14,7 @@
 #' @param nums The vector of numbers to be mapped
 #' @param start The start of the interval
 #' @param stop The end of the interval
+#' @param na.rm Whether to remove \code{NA} values before calculating interval
 #' @return The original numbers mapped to the given interval
 #' @author Jared P. Lander
 #' www.jaredlander.com
@@ -25,16 +26,16 @@
 #' MapToInterval(1:10, start=0, stop=1)
 #' mapping(1:10, start=0, stop=1)
 #' 
-MapToInterval <- function(nums, start=1, stop=10)
+MapToInterval <- function(nums, start=1, stop=10, na.rm=TRUE)
 {
     #do the mapping: a + (x - min(x)) * (b - a) / (max(x) - min(x))
-    mapped <- start + (nums - min(nums)) * (stop - start) / diff(range(nums))
+    mapped <- start + (nums - min(nums, na.rm=na.rm)) * (stop - start) / diff(range(nums, na.rm=na.rm))
     return(mapped)
 }
 
 
 # just a better name for the function
-mapping <- function(nums, start=1, stop=10)
+mapping <- function(nums, start=1, stop=10, na.rm=FALSE)
 {
-    return(MapToInterval(nums=nums, start=start, stop=stop))
+    return(MapToInterval(nums=nums, start=start, stop=stop, na.rm=na.rm))
 }
